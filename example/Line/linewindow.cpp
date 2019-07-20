72cfc5152567f87a0717c5ac70aaa41ef66de32e
gandis <gandis0713@gmail.com>
2019-07-20 12:14:31 +0900

    Resolve build error

--- example/Line/linewindow.cpp
+++ example/Line/linewindow.cpp
@@ -1,13 +1,20 @@
 #include "linewindow.h"
 #include "ui_linewindow.h"
 
+#include "coqtrenderer.h"
+#include "coorthographiccamera.h"
+
 LineWindow::LineWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::LineWindow)
 {
     ui->setupUi(this);
 
-    CoQtRenderer m_pRender = new CoQtRenderer(ui->widget);
+    CoQtRenderer *m_pRender = new CoQtRenderer(ui->widget);
+
+    CoOrthographicCamera* m_pCamera = new CoOrthographicCamera();
+
+    m_pRender->setCamera(m_pCamera);
 }
 
 LineWindow::~LineWindow()
