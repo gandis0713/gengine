fca0c41666a72125ff63a78149ea97483be21b1d
gandis <gandis0713@gmail.com>
2019-07-21 10:16:47 +0900

    modified line example

--- example/Line/linewindow.cpp
+++ example/Line/linewindow.cpp
@@ -3,6 +3,7 @@
 
 #include "coqtrenderer.h"
 #include "coorthographiccamera.h"
+#include "coline.h"
 
 LineWindow::LineWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,10 +12,17 @@ LineWindow::LineWindow(QWidget *parent) :
     ui->setupUi(this);
 
     CoQtRenderer *m_pRender = new CoQtRenderer(ui->widget);
-
     CoOrthographicCamera* m_pCamera = new CoOrthographicCamera();
 
     m_pRender->setCamera(m_pCamera);
+
+    CoLine *pLine1 = new CoLine(CoVec3( -1.0f, -1.0f, 1.0f), CoVec3( 1.0f, -1.0f, 1.0f));
+    CoLine *pLine2 = new CoLine(CoVec3( 1.0f, -1.0f, 1.0f), CoVec3( 0.0f, 1.0f, 1.0f));
+    CoLine *pLine3 = new CoLine(CoVec3( 0.0f, 1.0f, 1.0f), CoVec3( -1.0f, -1.0f, 1.0f));
+
+    m_pRender->addNode(pLine1);
+    m_pRender->addNode(pLine2);
+    m_pRender->addNode(pLine3);
 }
 
 LineWindow::~LineWindow()
