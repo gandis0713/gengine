36c486729025180db348d70d07301ae19c31f702
gandis <gandis0713@gmail.com>
2019-08-11 18:43:51 +0900

    implemented that face index class

--- src/common/core/cofaceindex.cpp
+++ src/common/core/cofaceindex.cpp
@@ -4,3 +4,29 @@ CoFaceIndex::CoFaceIndex()
 {
 
 }
+
+CoFaceIndex::~CoFaceIndex()
+{
+
+}
+
+void CoFaceIndex::addVertexIndices(Guint vVertex)
+{
+    m_vecVertexIndices.push_back(vVertex);
+}
+
+void CoFaceIndex::addUICoordIndices(Guint vUICoord)
+{
+    m_vecUVCoordIndices.push_back(vUICoord);
+}
+
+void CoFaceIndex::addVertexNormalIndices(Guint vVertexNormal)
+{
+    m_vecVertexNormalIndices.push_back(vVertexNormal);
+}
+
+
+void CoFaceIndex::getVertexIndices(std::vector<Guint> &vecVertexIndices)
+{
+    vecVertexIndices = m_vecVertexIndices;
+}
--- src/common/core/cofaceindex.h
+++ src/common/core/cofaceindex.h
@@ -1,11 +1,29 @@
 #ifndef COFACEINDEX_H
 #define COFACEINDEX_H
 
+#include "covector2.h"
+#include "covector3.h"
 
-class CoFaceIndex
+#include "deengine.h"
+
+#include <vector>
+
+class GANDISENGINE CoFaceIndex
 {
 public:
     CoFaceIndex();
+    ~CoFaceIndex();
+
+    void addVertexIndices(Guint vVertex);
+    void addUICoordIndices(Guint vUICoord);
+    void addVertexNormalIndices(Guint vVertexNormal);
+
+    void getVertexIndices(std::vector<Guint> &vecVertexIndices);
+
+private:
+    std::vector<Guint> m_vecVertexIndices;
+    std::vector<Guint> m_vecUVCoordIndices;
+    std::vector<Guint> m_vecVertexNormalIndices;
 };
 
 #endif // COFACEINDEX_H
