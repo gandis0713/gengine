c931cfc5ecf33f68eacd987fa5d607e857f51f8d
gandis <gandis0713@gmail.com>
2019-08-11 18:40:55 +0900

    create class for define face index

---
+++ src/common/core/cofaceindex.cpp
@@ -0,0 +1,6 @@
+#include "cofaceindex.h"
+
+CoFaceIndex::CoFaceIndex()
+{
+
+}
---
+++ src/common/core/cofaceindex.h
@@ -0,0 +1,11 @@
+#ifndef COFACEINDEX_H
+#define COFACEINDEX_H
+
+
+class CoFaceIndex
+{
+public:
+    CoFaceIndex();
+};
+
+#endif // COFACEINDEX_H
