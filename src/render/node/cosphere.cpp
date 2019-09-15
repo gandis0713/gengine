336397d45ec9eadd2cffac8a9662f59de815a7f7
gandis <gandis0713@gmail.com>
2019-09-15 17:36:09 +0900

    implemented that create vertex indices for sphere

--- src/render/node/cosphere.cpp
+++ src/render/node/cosphere.cpp
@@ -27,41 +27,62 @@ CoSphere::~CoSphere()
 
 }
 
+/**
+ * @brief This function is used to opengl coordinate opengl to create vertice of sphere.
+ *        z-axis is screen.
+ *        y-axis is up.
+ *        x-aixs is right.
+ */
 void CoSphere::createSphere()
 {
+    // step 1 : create vertice
 
     Gint n2PIDegree = 360;
-    Gint nSectorRatio1 = 10;
-    Gint nSectorRatio2 = 10;
-    Gint nSector1 = n2PIDegree / nSectorRatio1 / 2;
-    Gint nSector2 = n2PIDegree / nSectorRatio2;
+    Gint nStackRatio = 10;
+    Gint nSectorRatio = 10;
+    Gint nStack = n2PIDegree / 2 / nStackRatio;
+    Gint nSector = n2PIDegree / nSectorRatio;
 
     CoVec3 startPosition(m_vCenter[0], m_vCenter[1] + m_fRadius, m_vCenter[2]);
     CoVec3 startNormal = startPosition - m_vCenter;
 
-    vector<vector<CoVec3>> vecSectors;
-    for(Gint i = 0; i < nSector1; i++)
+    vector<CoVec3> vecVertices;
+    for(Gint i = 0; i < nStack; i++)
     {
-        Gfloat fTheta1 = i * nSectorRatio1;
+        Gfloat fStackTheta = i * nStackRatio;
 
-        vector<CoVec3> vecSector1;
-
-        for(Gint j = 0; j < nSector2; j++)
+        for(Gint j = 0; j < nSector; j++)
         {
-            Gfloat fTheta2 = j * nSectorRatio2;
+            Gfloat fSectorTheta = j * nSectorRatio;
 
-            Gfloat x = startNormal.length() * sin(fTheta1 * DEG2RAD) * sin(fTheta2 * DEG2RAD);
-            Gfloat y = startNormal.length() * cos(fTheta1 * DEG2RAD);
-            Gfloat z = startNormal.length() * sin(fTheta1 * DEG2RAD) * cos(fTheta2 * DEG2RAD);
+            Gfloat x = startNormal.length() * sin(fStackTheta * DEG2RAD) * sin(fSectorTheta * DEG2RAD);
+            Gfloat y = startNormal.length() * cos(fStackTheta * DEG2RAD);
+            Gfloat z = startNormal.length() * sin(fStackTheta * DEG2RAD) * cos(fSectorTheta * DEG2RAD);
 
             CoVec3 vPos(x, y, z);
 
-            vecSector1.push_back(vPos);
+            vecVertices.push_back(vPos);
         }
-
-        vecSectors.push_back(vecSector1);
     }
 
+    // step 2 : create indices for triangle.
+
+    vector<Gint> vecVertexIndices;
+    for(Gint i = 0; i < nStack - 1; i++)
+    {
+        for(Gint j = 0; j < nSector; j++)
+        {
+            // triangle 1
+            vecVertexIndices.push_back(i * nSector + j); // stack 0 + sector 0
+            vecVertexIndices.push_back((i + 1) * nSector + j); // stack 1 + sector 0
+            vecVertexIndices.push_back(i * nSector + j + 1); // stack 0 + sector 1
+
+            // triangle 2
+            vecVertexIndices.push_back(i * nSector + j + 1); // stack 0 + sector 1
+            vecVertexIndices.push_back((i + 1) * nSector + j); // stack 1 + sector 0
+            vecVertexIndices.push_back((i + 1) * nSector + j + 1); // stack 1 + sector 1
+        }
+    }
 }
 
 void CoSphere::setCenter(CoVec3 vPosition)
