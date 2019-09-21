e4dc5e662c94ae7c3405aec174c7957f89415054
gandis <gandis0713@gmail.com>
2019-09-21 20:10:20 +0900

    creating example for cone

---
+++ example/cone/main.cpp
@@ -0,0 +1,26 @@
+#include "samplewindow.h"
+#include "coqtrenderer.h"
+#include "coorthographiccamera.h"
+#include "cocone.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    CSampleWindow w;
+
+    CoQtRenderer *m_pRender = new CoQtRenderer(w.getMainWidget());
+    CoOrthographicCamera* m_pCamera = new CoOrthographicCamera();
+
+    m_pRender->setCamera(m_pCamera);
+
+    CoCone *pCone = new CoCone(CoVec3(0,0,0), 10, 30);
+
+    m_pRender->addNode(pCone);
+
+    w.setQtRenderer(m_pRender);
+    w.show();
+
+    return a.exec();
+}
