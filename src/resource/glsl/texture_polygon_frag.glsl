7fc44bcf04a7b777e2a93d48285088a82b2babe2
gandis <gandis0713@gmail.com>
2019-09-26 00:17:25 +0900

    The glsl for textured polygon was modified

--- src/resource/glsl/texture_polygon_frag.glsl
+++ src/resource/glsl/texture_polygon_frag.glsl
@@ -20,40 +20,5 @@ uniform sampler2D texture2D;
 
 void main()
 {
-    // Material properties
-    vec3 texture_diffuse_color = texture( texture2D, vertex_UV ).rgb;
-    vec3 ambient_diffuse_color = ambientColor * texture_diffuse_color;
-
-    // Distance to the light
-    float distance = length( lightPosition - vertex_w );
-
-    // Normal of the computed fragment, in camera space
-    vec3 n = normalize( vertexNormal_c );
-    // Direction of the light (from the fragment to the light)
-    vec3 l = normalize( lightDir_c );
-    // Cosine of the angle between the normal and the light direction,
-    // clamped above 0
-    //  - light is at the vertical of the triangle -> 1
-    //  - light is perpendicular to the triangle -> 0
-    //  - light is behind the triangle -> 0
-    float cosTheta = clamp( dot( n,l ), 0,1 );
-    //        float cosTheta = dot( n,l );
-
-    // Eye vector (towards the camera)
-    vec3 E = normalize(eyeDir_c);
-    // Direction in which the triangle reflects the light
-    vec3 R = reflect(-l,n);
-    // Cosine of the angle between the Eye vector and the Reflect vector,
-    // clamped to 0
-    //  - Looking into the reflection -> 1
-    //  - Looking elsewhere -> < 1
-    float cosAlpha = clamp( dot( E,R ), 0,1 );
-
-    color =
-            // Ambient : simulates indirect lighting
-            ambient_diffuse_color +
-            // Diffuse : "color" of the object
-            texture_diffuse_color * lightColor * lightPower * cosTheta / (distance*distance) +
-            // Specular : reflective highlight, like a mirror
-            specularColor * lightColor * lightPower * pow(cosAlpha,5) / (distance*distance);
+    color = texture( texture2D, vertex_UV ).rgb;
 }
